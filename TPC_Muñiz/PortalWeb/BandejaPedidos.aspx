15481a0f6c11963f27628d4fb699be12b8fd8be7
Facundo Muñiz <facundomartinmuniz@gmail.com>
2019-06-06 23:24:51 -0300

    Se agrega proyecto web y se desarrolla mas la base de datos. tambien se le agega la clase conexion a base de datos

---
+++ TPC_Muñiz/PortalWeb/BandejaPedidos.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="BandejaPedidos.aspx.cs" Inherits="PortalWeb.BandejaPedidos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+    <asp:UpdatePanel ID="upBandejaHojasDeRuta_Filtrar" runat="server">
+        <ContentTemplate>
+            <asp:Panel ID="pnlFiltrarHojasDeRuta" runat="server" DefaultButton="btnFiltrar">
+                <div class="form-inline justify-content-center mt-2">
+                    <div class="form-group">
+                        <label class="sr-only">Hoja de Ruta</label>
+                        <asp:TextBox CssClass="form-control" ID="txtHojaDeRuta" runat="server" placeholder="Hoja de Ruta"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only">Fecha</label>
+                        <p>
+                            <input type="text" autocomplete="off" id="datepicker" runat="server" class="mt-3 form-control" placeholder="Fecha (DD/MM/AAAA)"></p>
+
+                    </div>
+                    <div class="btn-group ml-1" role="group" aria-label="Botones filtrar">
+                        <asp:Button ID="btnFiltrar" type="submit" runat="server" CssClass="btn btn-blue" Text="Filtrar" OnClick="btnFiltrar_Click"></asp:Button>
+                        <asp:Button ID="btnLimpiarFiltros" type="submit" runat="server" CssClass="btn btn-secondary" Text="Limpiar Filtros" OnClick="btnLimpiarFiltros_Click"></asp:Button>
+                    </div>
+                </div>
+            </asp:Panel>
+
+          <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <asp:Panel ID="Panel1" runat="server" DefaultButton="btnFiltrar">
+                <div class="form-inline justify-content-center mt-2">
+                    <div class="form-group">
+                        <label class="sr-only">Hoja de Ruta</label>
+                        <asp:TextBox CssClass="form-control" ID="TextBox1" runat="server" placeholder="Hoja de Ruta"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only">Fecha</label>
+                        <p>
+                            <input type="text" autocomplete="off" id="Text1" runat="server" class="mt-3 form-control" placeholder="Fecha (DD/MM/AAAA)"></p>
+
+                    </div>
+                    <div class="btn-group ml-1" role="group" aria-label="Botones filtrar">
+                        <asp:Button ID="Button1" type="submit" runat="server" CssClass="btn btn-blue" Text="Filtrar" OnClick="btnFiltrar_Click"></asp:Button>
+                        <asp:Button ID="Button2" type="submit" runat="server" CssClass="btn btn-secondary" Text="Limpiar Filtros" OnClick="btnLimpiarFiltros_Click"></asp:Button>
+                    </div>
+                </div>
+            </asp:Panel>
+
+            <div class="ml-2">
+                <div class="table-responsive">
+              <%--  <asp:GridView ID="gvHojasDeRuta" runat="server" CssClass="mt-2" Font-Size="10pt" OnSorting="GridView_Sorting" OnRowDataBound="GridView_RowDataBound" OnPageIndexChanging="GridView_PageIndexChanging">
+                  
+                </asp:GridView>--%>
+                    </div>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+
+
+</asp:Content>
---
+++ TPC_Muñiz/PortalWeb/BandejaPedidos.aspx.designer.cs
@@ -0,0 +1,123 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PortalWeb {
+    
+    
+    public partial class BandejaPedidos {
+        
+        /// <summary>
+        /// Control upBandejaHojasDeRuta_Filtrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel upBandejaHojasDeRuta_Filtrar;
+        
+        /// <summary>
+        /// Control pnlFiltrarHojasDeRuta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlFiltrarHojasDeRuta;
+        
+        /// <summary>
+        /// Control txtHojaDeRuta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtHojaDeRuta;
+        
+        /// <summary>
+        /// Control datepicker.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText datepicker;
+        
+        /// <summary>
+        /// Control btnFiltrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnFiltrar;
+        
+        /// <summary>
+        /// Control btnLimpiarFiltros.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLimpiarFiltros;
+        
+        /// <summary>
+        /// Control UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
+        /// Control Panel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Control Text1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText Text1;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Control Button2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+    }
+}
